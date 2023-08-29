a18572e996b03608b4a3335199a1e8923e6bf608
Yuuki310 <yuuki310univ@gmail.com>
2023-08-30 01:15:26 +0900

    2nd

--- model.py
+++ model.py
@@ -13,15 +13,9 @@ class Net(nn.Module):
         
         self.network = nn.Sequential(
             self.encoder(),
-            self.one_d_block(128, self.L),
-#            nn.Conv1d(128, 256, 32),
+            self.one_d_block(N=128),
             nn.AdaptiveAvgPool2d(1)
         )
-        # self.bottleneck1 = nn.Conv1d(1, 8,  kernel_size=1, stride=1, bias=False)
-        # self.conv1 = nn.Conv1d(1, 16, kernel_size=L, stride=L // 2, bias=False)
-        # self.conv2 = nn.Conv1d(16, 1, kernel_size=L, stride=L // 2 ,bias=False)
-        # # self.conv3 = nn.Conv1d(32, 16, kernel_size=L, stride=L // 2, bias=False)
-        # # self.conv4 = nn.Conv1d(16, 1, kernel_size=L, stride=L // 2 ,bias=False)
         
 
         # self.fc1 = nn.Linear(32 * 5 * 5, 120)
@@ -29,14 +23,21 @@ class Net(nn.Module):
 
         # self.ave = nn.AdaptiveAvgPool2d(1)
 
-    def one_d_block(self, d, L, ):
+    def one_d_block(self, N=128, L=32, B=128):
         block = nn.Sequential(
-            nn.Conv1d(d, d//2, kernel_size=L, stride=1),
+            # M : batchsize
+            # bottleneck
+            # [M, N, K] -> [M, B, K]
+            nn.Conv1d(N, B, 1, bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(d//2),
-            nn.Conv1d(d//2, d, L, stride=L//2),
+            nn.BatchNorm1d(B),
+            #[M, B, K] -> [M, B, K]
+            nn.Conv1d(B, B, L, stride=L//2),
             nn.ReLU(),
-            nn.BatchNorm2d(d),    
+            nn.BatchNorm1d(B),    
+            #[M, B, K] -> [M, N, K] 
+            nn.Conv1d(B, N, 1, bias=False),
+
         )
         return block
     
@@ -49,24 +50,7 @@ class Net(nn.Module):
         return encoder
     
     def forward(self, x):
-        # x = self.bottleneck1(x)
-        # x = self.relu(x)
-        # x = self.pool(x)
-        
         x = self.network(x)
-        # x = self.conv3(x)
-        # x = self.relu(x)
-        # x = self.pool(x)
-        
-        # x = self.conv4(x)
-        # x = self.relu(x)
-        # x = self.pool(x)
-
-        # x = self.conv3(x)
-        # x = self.fc1(x)
-        # x = self.relu(x)
-        # x = self.fc2(x)
-        # x = x.view(x.size()[0], -1)
         return x
     
 if __name__ == "__main__":
